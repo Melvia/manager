960bdf63bd54cb4cf29d5d8c55a1b53e9e4b1c1c
Nata <melvia.nr@gmail.com>
2024-07-21 21:48:17 +0500

    products

---
+++ src/main/java/ag/selm/manager/repository/InMemoryProductRepository.java
@@ -0,0 +1,19 @@
+package ag.selm.manager.repository;
+
+import ag.selm.manager.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+@Repository
+public class InMemoryProductRepository implements ProductRepository {
+
+    private final List<Product> products = Collections.synchronizedList(new LinkedList<>());
+
+    @Override
+    public List<Product> findAll() {
+        return this.products;
+    }
+}
---
+++ src/main/java/ag/selm/manager/service/ProductService.java
@@ -0,0 +1,9 @@
+package ag.selm.manager.service;
+
+import ag.selm.manager.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> findAllProducts();
+}

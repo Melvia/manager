ec82f2bb08c5afd9e3b7b9de7a9101df3857e4c6
Nata <melvia.nr@gmail.com>
2024-07-30 23:22:03 +0500

    products-template-2

--- src/main/java/ag/selm/manager/controller/ProductsController.java
+++ src/main/java/ag/selm/manager/controller/ProductsController.java
@@ -1,11 +1,12 @@
 package ag.selm.manager.controller;
 
+import ag.selm.manager.controller.payload.NewProductPayload;
+import ag.selm.manager.entity.Product;
 import ag.selm.manager.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,10 +15,25 @@ public class ProductsController {
 
     private final ProductService productService;
 
-    @RequestMapping(value = "list", method = RequestMethod.GET)
+
+
+    @GetMapping("list")
     public String getProductsList(Model model) {
-        model.addAttribute("product", this.productService.findAllProducts());
+        model.addAttribute("products", this.productService.findAllProducts());
         return "catalogue/products/list";
 
     }
+
+    @GetMapping("create")
+    public String getNewProductPage() {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(NewProductPayload payload) {
+        Product product = this.productService.createProduct(payload.title(), payload.details());
+        return "redirect:/catalogue/products/%d".formatted(product.getId());
+
+    }
+
 }
--- src/main/java/ag/selm/manager/repository/InMemoryProductRepository.java
+++ src/main/java/ag/selm/manager/repository/InMemoryProductRepository.java
@@ -3,9 +3,7 @@ package ag.selm.manager.repository;
 import ag.selm.manager.entity.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 @Repository
@@ -21,5 +19,21 @@ public class InMemoryProductRepository implements ProductRepository {
     @Override
     public List<Product> findAll() {
         return Collections.unmodifiableList(this.products);
+    }
+
+    @Override
+    public Product save(Product product) {
+        product.setId(this.products.stream()
+                .max(Comparator.comparingInt(Product::getId))
+                .map(Product::getId)
+                .orElse(0) + 1);
+        this.products.add(product);
+        return product;
+    }
+    @Override
+    public Optional<Product> findById(Integer productId) {
+        return this.products.stream()
+                .filter(product -> Objects.equals(productId, product.getId()))
+                .findFirst();
     }
 }
